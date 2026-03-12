7678fe723a02a64bc3522118faa2a547ce12c06d
TONNY-tec <tonnysafari3@gmail.com>
2026-03-12 11:01:16 +0000

    added the camera for scanning food gradients in there packeged food

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 
 # --- Gemini AI Configuration ---
 # Replace the string below with your actual API key from Google AI Studio
-GEMINI_API_KEY = 'AIzaSyCb5v_jm5XnpCCtQ4K2WVwLPXe4hi40E-o' 
+GEMINI_API_KEY = '' 
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 # Optional: Set a default model version if you plan to use it in multiple places
 GEMINI_MODEL_NAME = "gemini-2.5-flash"
